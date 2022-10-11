7cd14dce759cba57e33df4bbe7aead32258d6779
henry-kiet <kietyasuo2016@gmail.com>
2022-10-11 08:44:44 +0700

    add note TestJunit

--- test/com/sof/equation/core/FirstOrderEquationTestJunit.java
+++ test/com/sof/equation/core/FirstOrderEquationTestJunit.java
@@ -6,7 +6,6 @@ package com.sof.equation.core;
 
 import org.junit.Assert;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 /**
  *
--- test/com/sof/equation/core/QuadraticEquationTestJunit.java
+++ test/com/sof/equation/core/QuadraticEquationTestJunit.java
@@ -24,23 +24,30 @@ public class QuadraticEquationTestJunit {
         Assert.assertArrayEquals(expected3, QuadraticEquation.calculateQuadraticEquation(2, 0, -4), 0);
     }
     
+    
+    
+    @Test(expected = IllegalArgumentException.class)
+    public void testQuadraticEquationGivenWtrongExpectedReturnException(){
+        //trường hợp expected đặt sai vị trí 
+        //cách fix: 1. thay đổi vị trí 2 số (cực mà hàm lại không chắc chắn)
+        //          2. sửa lại hàm calculateQuadraticEquation 
+        double[] expected = {2 , 6};
+        Assert.assertArrayEquals(expected, QuadraticEquation.calculateQuadraticEquation(1, -8, 12), 0);
+        //trường hợp expected do tester nhập sai
+        //double[] expected2 = {-5};
+        //tester lại nhập là -5 ->  nghiệm là {-2} 
+        //Assert.assertArrayEquals(expected2, QuadraticEquation.calculateQuadraticEquation(2, 8, 8), 0);
+    }
+    
     @Test(expected = IllegalArgumentException.class)
     public void testQuadraticEquationGivenWtrongArgumentReturnException(){
+        //bắt lỗi delta < 0
         QuadraticEquation.calculateQuadraticEquation(1, 2, 3);
     }
     
-//    @Test
-//    public void testQuadraticEquationGivenWtrongExpectedReturnException(){
-//        //trường hợp expected đặt sai vị trí 
-//        double[] expected = {2 , 6};
-//        Assert.assertArrayEquals(expected, QuadraticEquation.calculateQuadraticEquation(1, -8, 12), 0);
-////        double[] expected2 = {-5};
-////        Assert.assertArrayEquals(expected2, QuadraticEquation.calculateQuadraticEquation(2, 8, 8), 0);
-//    }
-    
-    
     @Test(expected = IllegalArgumentException.class)
     public void testQuadraticEquationGivenWtrongArgumentReturnException2(){
+        //bắt lỗi a < 0
         QuadraticEquation.calculateQuadraticEquation(0, 0, 0);
         QuadraticEquation.calculateQuadraticEquation(0, 0, 5);
         QuadraticEquation.calculateQuadraticEquation(0, 2, 8);
