0b2d73cdc768eab9241893c5758e3b6be18d92d9
Nguyễn Văn Duy <113020285+Dalaney-D@users.noreply.github.com>
2022-10-17 22:07:55 +0700

    Revert "update name packages test"

--- test/com/sof/equation/core/QuadraticEquationTestJunit.java
+++ test/com/sof/equation/core/QuadraticEquationTestJunit.java
@@ -2,9 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
  */
-package com.sof.equation.JUnit;
+package com.sof.equation.core;
 
-import com.sof.equation.core.QuadraticEquation;
 import org.junit.Assert;
 import org.junit.Test;
 
